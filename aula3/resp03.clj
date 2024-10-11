a5ae47b137a35f1f070e53227b117b8a32247e4f
Nicole Brito Maracajá <139410383+nicolemaracaja@users.noreply.github.com>
2024-10-11 10:45:49 -0300

    Create resp03.clj

---
+++ aula3/resp03.clj
@@ -0,0 +1,17 @@
+(defn somador [op]
+  (fn [seq]
+    (if (empty? seq) 0
+    (+ (op (first seq)) ((somador op) (rest seq))))))
+
+(def soma-cubos (somador (fn [n] (* n n n))))
+(def soma-dobros (somador (fn [n] (* n 2))))
+(def soma-triplos (somador (fn [n] (* n 3))))
+(def soma (somador (fn [n] n)))
+(def conta (somador (fn [n] (+ 1))))
+
+;;Testes do somador
+(assert (= 36 (soma-cubos [1 2 3])))
+(assert (= 12 (soma-dobros [1 2 3])))
+(assert (= 18 (soma-triplos [1 2 3])))
+(assert (= 6 (soma [1 2 3])))
+(assert (= 3 (conta [1 2 3])))
